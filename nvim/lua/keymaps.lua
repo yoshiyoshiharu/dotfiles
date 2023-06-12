1d33c3ca37f6eb091b69e1a8f21ae5118c645a6b
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2023-06-13 08:43:47 +0900

    fix keymap

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -8,6 +8,7 @@ keymap.set("t", "<Esc>", "<C-\\><C-n>")
 keymap.set("i", "jj", "<ESC>")
 keymap.set("n", "<C-q>", ":q<cr>")
 keymap.set("i", "<C-q>", "<ESC>:q<cr>")
+keymap.set("t", "<C-q>", "<ESC>:q<cr>")
 
 -- delete single character without copying into register
 keymap.set("n", "x", '"_x')
