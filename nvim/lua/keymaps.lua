1ee940caeefec01b8d4f27dcd3e1fd36e44f3c71
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2023-06-14 14:44:51 +0900

    fix keymap

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -16,9 +16,8 @@ keymap.set("n", "<leader>l", "$")
 keymap.set("n", "x", '"_x')
 
 -- window management
-keymap.set("n", "<leader>sv", "<C-w>v") -- split window vertically
-keymap.set("n", "<leader>sh", "<C-w>s") -- split window horizontally
-keymap.set("n", "<leader>se", "<C-w>=") -- make split windows equal width & height
+keymap.set("n", "<leader>sl", "<C-w>v") -- split window vertically
+keymap.set("n", "<leader>sj", "<C-w>s") -- split window horizontally
 keymap.set("n", "<leader>sx", ":close<CR>") -- close current split window
 
 keymap.set("n", "<leader>to", ":tabnew<CR>") -- open new tab
