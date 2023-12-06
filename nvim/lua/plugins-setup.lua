a80614c3589064bd496fb0c432738f31460442ad
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2023-12-06 11:17:14 +0900

    add octo nvim

--- nvim/lua/plugins-setup.lua
+++ nvim/lua/plugins-setup.lua
@@ -149,6 +149,18 @@ return require('packer').startup(function(use)
     end,
   }
 
+  use {
+    'pwntester/octo.nvim',
+    requires = {
+      'nvim-lua/plenary.nvim',
+      'nvim-telescope/telescope.nvim',
+      'nvim-tree/nvim-web-devicons',
+    },
+    config = function ()
+      require"octo".setup()
+    end
+  }
+
   if packer_bootstrap then
     require("packer").sync()
   end
