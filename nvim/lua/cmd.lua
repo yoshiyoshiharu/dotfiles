198a799bbeefd7f6f8ab16966ebee6ff433c44f2
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2024-01-14 14:52:00 +0900

    fix cp file path to copy relative path

--- nvim/lua/cmd.lua
+++ nvim/lua/cmd.lua
@@ -7,3 +7,12 @@ autocmd("TermOpen", {
 })
 
 vim.api.nvim_create_user_command('Ghpr', '!gh pr list --web -s all -S <q-args>', { nargs = 1 })
+
+vim.api.nvim_create_user_command("Copypath", function()
+  local absolute_file_path = vim.fn.expand("%:p")
+  local git_root = vim.fn.systemlist("git rev-parse --show-toplevel")[1]
+  local relative_file_path = string.sub(absolute_file_path, string.len(git_root) + 2)
+
+  vim.fn.setreg("+", relative_file_pathpath)
+  vim.notify('Copied "' .. relative_file_path .. '" to the clipboard!')
+end, {})
