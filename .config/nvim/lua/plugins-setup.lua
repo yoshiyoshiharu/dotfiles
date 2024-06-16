894d3c2cf484c9550b7a14012d4a9cb60da37bf6
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2024-06-16 15:42:13 +0900

    add md preview

--- .config/nvim/lua/plugins-setup.lua
+++ .config/nvim/lua/plugins-setup.lua
@@ -99,4 +99,10 @@ require("lazy").setup({
     end,
   },
   'ruifm/gitlinker.nvim',
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function() vim.fn["mkdp#util#install"]() end,
+  }
 })
