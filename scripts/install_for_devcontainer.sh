16f1975d7312b62d0cebab9c2975d849322bd3a9
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2024-10-05 17:38:14 +0900

    add devcontainer installer

--- scripts/install_for_devcontainer.sh
+++ scripts/install_for_devcontainer.sh
@@ -1,3 +1,10 @@
-#!/bin/bash
+#!/bin/bash -e
 
-echo "hellooooooooooo"
+function setup_zshrc() {
+  rm -rf $HOME/.zsh/antigen/
+  git clone https://github.com/zsh-users/antigen.git $HOME/.zsh/antigen/
+  exec $SHELL
+}
+
+echo "--------------setup .zshrc--------------"
+setup_zshrc
