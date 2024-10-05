d9f42f023502a01393376aa1143459260abce883
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2024-10-05 22:17:28 +0900

    delete markdown-preview

--- .config/nvim/lua/plugins-setup.lua
+++ .config/nvim/lua/plugins-setup.lua
@@ -99,10 +99,4 @@ require("lazy").setup({
     end,
   },
   'ruifm/gitlinker.nvim',
-  {
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    ft = { "markdown" },
-    build = function() vim.fn["mkdp#util#install"]() end,
-  }
 })
