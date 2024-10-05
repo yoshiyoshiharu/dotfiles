56cfc80f366228c02916588a4db35f235f50c27f
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2024-10-05 18:21:02 +0900

    add devcontainer start sh

---
+++ .devcontainer/start.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+devcontainer up --workspace-folder . \
+                --additional-features='{"ghcr.io/duduribeiro/devcontainer-features/neovim:1": {}, "ghcr.io/georgofenbeck/features/lazygit-linuxbinary:1": {}}' \
+                --dotfiles-repository "https://github.com/yoshiyoshiharu/dotfiles" \
+                --dotfiles-install-command "./scripts/install_for_devcontainer.sh"
