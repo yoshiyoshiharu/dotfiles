8f78d79bccc101780e33f6277b6df505534598fb
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2024-10-05 18:28:22 +0900

    move devcontainer setting files

--- .devcontainer/start.sh
+++ .devcontainer/start.sh
@@ -3,4 +3,4 @@
 devcontainer up --workspace-folder . \
                 --additional-features='{"ghcr.io/duduribeiro/devcontainer-features/neovim:1": {}, "ghcr.io/georgofenbeck/features/lazygit-linuxbinary:1": {}}' \
                 --dotfiles-repository "https://github.com/yoshiyoshiharu/dotfiles" \
-                --dotfiles-install-command "./scripts/install_for_devcontainer.sh"
+                --dotfiles-install-command "./.devcontainer/setup.sh"
