d8ce1af5eef8dac5d0b7e646c63358922333d1bd
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2024-10-12 16:14:40 +0900

    fix start.sh

--- .devcontainer/start.sh
+++ .devcontainer/start.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 devcontainer up --workspace-folder . \
-                --additional-features='{"ghcr.io/duduribeiro/devcontainer-features/neovim:1": {}, "ghcr.io/georgofenbeck/features/lazygit-linuxbinary:1": {}}' \
+                --additional-features='{
+                  "ghcr.io/duduribeiro/devcontainer-features/neovim:1": {},
+                  "ghcr.io/georgofenbeck/features/lazygit-linuxbinary:1": {},
+                  "ghcr.io/devcontainers/features/github-cli:1": {}
+                }' \
                 --dotfiles-repository "https://github.com/yoshiyoshiharu/dotfiles" \
                 --dotfiles-install-command "./.devcontainer/setup.sh"
