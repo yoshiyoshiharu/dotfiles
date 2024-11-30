13ed6dac2725321878b6d645dd0bb94feea41875
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2024-11-30 21:16:58 +0900

    delete lsp

--- .config/nvim/lua/plugins-setup.lua
+++ .config/nvim/lua/plugins-setup.lua
@@ -13,16 +13,4 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
   { import = "plugins" },
-  "williamboman/mason.nvim",
-  "williamboman/mason-lspconfig.nvim",
-  {
-    "glepnir/lspsaga.nvim",
-    branch = "main",
-    requires = {
-      "nvim-tree/nvim-web-devicons",
-      "nvim-treesitter/nvim-treesitter",
-    },
-  },
-  "jose-elias-alvarez/null-ls.nvim",
-  "jayp0521/mason-null-ls.nvim",
 })
