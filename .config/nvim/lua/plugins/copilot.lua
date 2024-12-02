03552ad9f3df145e8455b2598f257cd77274e497
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2024-12-02 13:08:46 +0900

    fix copilot

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -1,8 +1,4 @@
 return {
-  "zbirenbaum/copilot.lua",
-  cmd = "Copilot",
-  event = "InsertEnter",
-  config = function()
-    require("copilot").setup({})
-  end,
+  "github/copilot.vim",
+  lazy=false,
 }
