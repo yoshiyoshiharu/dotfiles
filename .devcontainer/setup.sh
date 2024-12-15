28f30cf999db1322b27e13eba5bb866436195c09
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2024-12-15 16:51:31 +0900

    fix setup

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -11,8 +11,10 @@ do
   ln -s $HOME/dotfiles/$file $HOME/$file
 done
 
+apt-get update
+apt-get install -y jq
+
 # install git-delta
 LATEST_GIT_DELTA_VERSION=$(curl -s https://api.github.com/repos/dandavison/delta/releases/latest | jq -r '.name')
-
 wget "https://github.com/dandavison/delta/releases/download/${LATEST_GIT_DELTA_VERSION}/git-delta-musl_${LATEST_GIT_DELTA_VERSION}_amd64.deb"
 apt-get install ./git-delta-musl_${LATEST_GIT_DELTA_VERSION}_amd64.deb
